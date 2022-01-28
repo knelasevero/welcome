40068c6ec814e39570dd2336b92ec47d4c2019d8
Lucas Severo Alves <lucassalves65@gmail.com>
2022-01-28 20:23:54 +0100

    ci: keep server if already confgured

--- deploy/scripts/write_kubeconfig.sh
+++ deploy/scripts/write_kubeconfig.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+export server=$(echo "${KUBECONFIGCONTENT}" | grep server)
+
 mkdir -p ~/.kube 
 touch ~/.kube/config
-echo "${KUBECONFIGCONTENT}" > ~/.kube/config
 chmod 600 ~/.kube/config
+
+if grep -q "$server" "$HOME/.kube/config"; then
+    echo "server already configured."
+else
+    echo "writing kubeconfig."
+    echo "${KUBECONFIGCONTENT}" > ~/.kube/config
+fi
+
